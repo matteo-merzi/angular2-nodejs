e4ab9696ff32b2c101de8360b1d703ff6fed5bb3
Matteo Merzi <matteo.merzi@gmail.com>
2017-02-11 21:48:08 +0100

    Deleted mongoose examples

--- routes/app.js
+++ routes/app.js
@@ -1,26 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function (err, user) {
-        if (err) {
-            return res.send('Error!');
-        }
-        res.render('node', {email: user.email});
-    });
-});
-
-router.post('/', function (req, res, next) {
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Matteo',
-        lastName: 'Merzi',
-        password: '123456',
-        email: email
-    });
-    user.save();
-    res.redirect('/');
+    res.render('index');
 });
 
 module.exports = router;
