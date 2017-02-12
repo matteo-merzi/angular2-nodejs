e305aa5fd3999ab5308572dce38b40168a037669
Matteo Merzi <matteo.merzi@gmail.com>
2017-02-12 12:33:44 +0100

    Added Signin and Signup form with validators

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 import {AppComponent} from "./app.component";
 import {MessageComponent} from "./messages/message.component";
@@ -30,7 +30,8 @@ import {SignupComponent} from "./auth/signup.component";
     imports: [
         BrowserModule,
         FormsModule,
-        routing
+        routing,
+        ReactiveFormsModule
     ],
     bootstrap: [AppComponent]
 })
