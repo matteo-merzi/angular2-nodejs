b26de552a2f93331c56b1dad68a46fead11e675a
Matteo Merzi <matteo.merzi@gmail.com>
2017-02-12 16:37:58 +0100

    Added message edit support

--- routes/messages.js
+++ routes/messages.js
@@ -37,4 +37,34 @@ router.post('/', function (req, res, next) {
     });
 });
 
+router.patch('/:id', function (req, res, next) {
+    Message.findById(req.params.id, function (err, message) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        if (!message) {
+            return res.status(500).json({
+                title: 'No Message found',
+                error: {message: 'Message not found'}
+            });
+        }
+        message.content = req.body.content;
+        message.save(function (error, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Message updated',
+                obj: result
+            })
+        });
+    });
+});
+
 module.exports = router;
