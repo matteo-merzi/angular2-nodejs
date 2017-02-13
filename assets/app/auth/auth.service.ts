4283681bb432c9595e2670c43799b5b0de3da8c1
Matteo Merzi <matteo.merzi@gmail.com>
2017-02-13 13:27:31 +0100

    Added logout user support

--- assets/app/auth/auth.service.ts
+++ assets/app/auth/auth.service.ts
@@ -26,4 +26,8 @@ export class AuthService {
             .map((response: Response) => response.json())
             .catch((error: Response) => Observable.throw(error.json()));
     }
+
+    logout() {
+        localStorage.clear();
+    }
 }
